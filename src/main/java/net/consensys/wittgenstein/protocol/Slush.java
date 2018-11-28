369e870533e00cf5d3a54a785471acda769bb7a0
vanessabridge <vane.m.bridge@gmail.com>
2018-11-28 14:57:48 -0500

    slush running but most nodes stay uncolored

--- src/main/java/net/consensys/wittgenstein/protocol/Slush.java
+++ src/main/java/net/consensys/wittgenstein/protocol/Slush.java
@@ -15,6 +15,7 @@ public class Slush implements Protocol {
   final Node.NodeBuilder nb;
   private static final int COLOR_NB = 2;
   private int count = 0;
+  private int actionCt = 0;
   /*
   m is the number of rounds
    */
@@ -26,7 +27,7 @@ public class Slush implements Protocol {
   /*
   A stands for the alpha treshold
    */
-  private final int AK = 4;
+  private static final int AK = 4;
   private static final int BYZANTINE_NODES_NB = 10;
   private Slush.SlushNode[] nodes = new Slush.SlushNode[NODES_AV];
 
@@ -53,25 +54,31 @@ public class Slush implements Protocol {
     return new Slush();
   }
 
-   class ReceiveQuery extends Network.Message<SlushNode> {
+  class ReceiveQuery extends Network.Message<SlushNode> {
 
     @Override
     public void action(SlushNode from, SlushNode to) {
-      count++;
+      //to.receiveQuery();
     }
   }
-  static class SendQuery extends Network.Message<SlushNode> {
 
+  static class SendQuery extends Network.Message<SlushNode> {
+    final Query querySent;
+
+    public SendQuery(Query query) {
+      this.querySent = query;
+    }
     @Override
     public void action(SlushNode from, SlushNode to) {
-      to.sendQuery();
+      to.sendQuery(querySent);
+      //to.receiveQuery();
     }
   }
 
   static class AnswerQuery extends Network.Message<SlushNode> {
     final Query queryAnswer;
 
-    public AnswerQuery(Query query){
+    public AnswerQuery(Query query) {
       this.queryAnswer = query;
     }
 
@@ -85,7 +92,7 @@ public class Slush implements Protocol {
     final int myId;
     int myColor = 0;
     int myQueryNonce;
-    final HashMap<Integer, Query> answerIP = new HashMap<>();
+    final HashMap<Integer, Answer> answerIP = new HashMap<>();
 
     public SlushNode(Random rd, NodeBuilder nb, boolean byzantine, int myId) {
       super(rd, nb, false);
@@ -103,7 +110,12 @@ public class Slush implements Protocol {
 
       return res;
     }
-    void sendAnswer(Query qa){++myQueryNonce;answerIP.put(myQueryNonce,qa);}
+
+    void sendAnswer(Query qa) {
+      ++myQueryNonce;
+      Answer aa = new Answer(qa.color,qa.answerId);
+      answerIP.put(myQueryNonce, aa);
+    }
 
     int otherColor() {
       return myColor == 1 ? 2 : 1;
@@ -114,23 +126,27 @@ public class Slush implements Protocol {
      * color, and initiates its own query, whereas a colored node simply responds with its current
      * color.
      */
-    public void receiveQuery(Query qa,SlushNode to) {
+    public void receiveQuery(Query qa, SlushNode to) {
       if (myColor == 0) {
         myColor = qa.color;
         //sendQuery(to,qa.color,qa.answerId);
-        //network.send(new SendQuery(),this,to);
-        sendQuery();
+        network.send(new SendQuery(qa),this,to);
       }
       //Send your color
-      network.send(new AnswerQuery(qa),this,to);
+      Answer qaaa = new Answer(to.myColor, qa.answerId);
+      network.send(new AnswerQuery(qaaa), to, this);
+
+     // receiveQueryAnswer(qaaa);
+
     }
+
     /*
      * Once the querying node collects k responses, it checks if a fraction ≥ αk are for the same
      * color, where α > 0.5 is a protocol parameter. If the αk threshold is met and the sampled
      * color differs from the node’s own color, the node flips to that color.
      */
     public void receiveQueryAnswer(Query qaa) {
-      Query asw = answerIP.get(qaa.answerId);
+      Answer asw = answerIP.get(qaa.answerId);
       asw.colorsFound[qaa.color]++;
       // in this case we assume that messages received correspond to the query answers
       if (asw.answerCount() == K) {
@@ -140,33 +156,41 @@ public class Slush implements Protocol {
         }
         count++;
       }
-      if(count < M){
-        sendQuery();
+      if (count < M) {
+        sendQuery(qaa);
       }
+      actionCt++;
     }
 
-    void sendQuery() {
+    void sendQuery(Query query) {
       ++myQueryNonce;
-      Query asw = new Query(this.myColor,this.myId);
+      Answer asw = new Answer(query.color,query.answerId);
       answerIP.put(myQueryNonce, asw);
       for (int r : getRandomRemotes()) {
-        network.send(new SendQuery(),this,nodes[r]);
+        receiveQuery(asw, nodes[r]);
       }
     }
-
   }
 
-  class Query {
+  static class Query {
 
     final int color;
     final int answerId;
-    private final int[] colorsFound = new int[COLOR_NB + 1];
 
-    Query(int color, int answerId){
+    Query(int color, int answerId) {
       this.color = color;
       this.answerId = answerId;
     }
 
+
+  }
+
+  static class Answer extends Query{
+
+    private final int[] colorsFound = new int[COLOR_NB + 1];
+    Answer(int color,int answerId){
+      super(color,answerId);
+    }
     int answerCount() {
       int sum = 0;
       for (int i : colorsFound)
@@ -174,32 +198,63 @@ public class Slush implements Protocol {
       return sum;
     }
   }
- 
-  public static void main(String... args) {
 
+  void stats() {
+    int std = 0;
+    int byz = 0;
+
+    for (SlushNode n : nodes) {
+      if (n.byzantine)
+        byz++;
+      else
+        std++;
+
+    }
+    System.out.println("N=" + nodes.length + ", K=" + K + ", AK=" + AK );
+    System.out.println("Nodes: standards=" + std + ", adversary=" + byz );
+
+
+    int[] res = new int[COLOR_NB + 1];
+    for (SlushNode n : nodes) {
+      res[n.myColor]++;
+    }
+
+    System.out.println("actions count:" + actionCt);
+    for (int i = 0; i < COLOR_NB + 1; i++) {
+      System.out.println(i + ":" + res[i]);
+    }
+  }
+  public static void main(String... args) {
     NetworkLatency nl = new NetworkLatency.NetworkLatencyByDistance();
 
     System.out.println("" + nl);
     //for (int i = 0; i < 1000; i++) {
-      //Instantiate protocol
-      Slush sp = new Slush();
-      // Set network latency
-      sp.network.setNetworkLatency(nl);
-      //Initialize with 100 nodes
-      sp.init();
-      SlushNode uncolored = sp.network().getNodeById(0);
-      List<Integer> kNodes = uncolored.getRandomRemotes();
-      System.out.println(kNodes.get(0));
-      int cnt = 0;
-      while (uncolored.getMsgReceived() < K) {
-        sp.network.send(new SendQuery(), uncolored, sp.network.getNodeById(kNodes.get(cnt)));
-        cnt++;
-      }
+    //Instantiate protocol
+    Slush sp = new Slush();
+    // Set network latency
+    sp.network.setNetworkLatency(nl);
+    //Initialize with 100 nodes
+    sp.init();
+    SlushNode uncolored1 = sp.network().getNodeById(0);
+    SlushNode uncolored2 = sp.network().getNodeById(1);
 
-      System.out.println("Finished");
+      sp.network.send(new SendQuery(new Query(1,-1)), uncolored2, uncolored1);
+      sp.network.send(new SendQuery(new Query(2,-1)), uncolored1, uncolored2);
+      sp.network.runMs(10000);
+    System.out.println("Finished");
 
+    System.out.println("N=" + sp.nodes.length + ", K=" + K + ", AK=" + AK );
+    int[] res = new int[COLOR_NB + 1];
+    for (SlushNode n : sp.nodes) {
+      res[n.myColor]++;
+    }
+
+    System.out.println("actions count:" + sp.actionCt);
+    for (int i = 0; i < COLOR_NB + 1; i++) {
+      System.out.println(i + ":" + res[i]);
+    }
     //}
 
-
   }
+
 }
